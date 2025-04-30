a38b9075b776139a3ddb6dcc7faa3b8b199e88b0
Sandro Francisco <sandro@Sandros-MacBook-Pro.local>
2025-04-30 14:20:29 +0100

    fixed l11 bug

--- ch1/L11.go
+++ ch1/L11.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var username string = "presidentSkroob"
-	var password int = 12345
+	var password string = "12345"
 
 	// don't edit below this line
 	fmt.Println("Authorization: Basic", username+":"+password)
