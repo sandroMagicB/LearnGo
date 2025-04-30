1a2c17f7600eace8cfe2ec095bd970bc643e5666
Sandro Francisco <sandro@Sandros-MacBook-Pro.local>
2025-04-30 16:19:19 +0100

    fixed error on file c1

--- ch1/C1.go
+++ ch1/C1.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var startup int = "Textio SMS service booting up..."
+	var startup string = "Textio SMS service booting up..."
 	var message string = "Sending test message"
 	var confirm string = "Message sent!"
 
 	// don't touch below this line
+package main
+
+import "fmt"
+
+func main() {
+	senderName = "Syl"
+	recipient = "Kaladin"
+	message = "The Words, Kaladin. You have to speak the Words!"
+
+	fmt.Printf("%s to %s: %s\n", senderName, recipient, message)
+}
 
 	fmt.Println(startup)
 	fmt.Println(message)
