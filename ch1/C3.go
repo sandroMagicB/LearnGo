f9d420f7569a28e3992a34acbb13db5890b737ac
Sandro Francisco <sandro@Sandros-MacBook-Pro.local>
2025-04-30 16:17:50 +0100

    created new bug fix files

---
+++ ch1/C3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	penniesPerText float64 := 2
+
+	// don't edit below this line
+	fmt.Printf("The type of penniesPerText is %T\n", penniesPerText)
+}
+
